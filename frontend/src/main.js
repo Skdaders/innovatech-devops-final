2ad8220e3fca25cbbedd8206efe0ce576aa04521
Skdaders <th.manquepan@duocuc.cl>
2026-05-25 15:21:30 -0400

    actualizacion mensaje de fronted

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,6 +7,6 @@ document.querySelector("#app").innerHTML = `
   <main style="font-family:system-ui;margin:2rem;">
     <h1>Frontend OK</h1>
     <p>VITE_API_URL (build-time): <code>${api || "(no definida)"}</code></p>
-    <p>Llamadas desde el navegador al backend suelen usar <code>http://localhost:3000</code> vía esta variable.</p>
+    <p>Este es nuestro Proyecto, Thomas manquepan, khaterine Diaz </p>
   </main>
 `;
